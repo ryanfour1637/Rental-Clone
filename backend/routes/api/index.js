102f3ee7594153dba91f5473ced94e982a3738d8
Ryan Fournier <ryanfour1637@gmail.com>
2023-08-11 10:14:04 -0700

    added delete a spot

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
 const reviewsRouter = require("./review.js");
 const bookingsRouter = require("./bookings.js");
+const spotImageRouter = require("./spot-image.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
@@ -18,6 +19,8 @@ router.use("/spots", spotsRouter);
 router.use("/reviews", reviewsRouter);
 
 router.use("/bookings", bookingsRouter);
+
+router.use("/spot-images", spotImageRouter);
 
 router.post("/test", (req, res) => {
    res.json({ requestBody: req.body });
