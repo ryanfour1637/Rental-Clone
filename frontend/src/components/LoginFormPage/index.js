30c0ec30a5283756207cf340807440f451dda327
Ryan Fournier <ryanfour1637@gmail.com>
2023-08-24 13:08:20 -0700

    added sign up thunk

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -16,12 +16,12 @@ function LoginFormPage() {
    const handleSubmit = (e) => {
       e.preventDefault();
       setErrors({});
-      return dispatch(sessionActions.login({ credential, password })).catch(
-         async (res) => {
-            const data = await res.json();
-            if (data && data.errors) setErrors(data.errors);
-         }
-      );
+      return dispatch(
+         sessionActions.thunkLogin({ credential, password })
+      ).catch(async (res) => {
+         const data = await res.json();
+         if (data && data.errors) setErrors(data.errors);
+      });
    };
 
    return (
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,38 +3,50 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
-const setUser = (user) => {
-   return {
-      type: SET_USER,
-      payload: user,
-   };
+const actionSetUser = (user) => {
+   return { type: SET_USER, payload: user };
 };
 
-const removeUser = () => {
-   return {
-      type: REMOVE_USER,
-   };
+const actionRemoveUser = () => {
+   return { type: REMOVE_USER };
 };
 
-export const login = (user) => async (dispatch) => {
+export const thunkLogin = (user) => async (dispatch) => {
    const { credential, password } = user;
-   const response = await csrfFetch("/api/session", {
+   const res = await csrfFetch("/api/session", {
       method: "POST",
       body: JSON.stringify({
          credential,
          password,
       }),
    });
-   const data = await response.json();
-   dispatch(setUser(data.user));
-   return response;
+   const data = await res.json();
+   dispatch(actionSetUser(data.user));
+   return res;
 };
 
-export const restoreUser = () => async (dispatch) => {
-   const response = await csrfFetch("/api/session");
-   const data = await response.json();
-   dispatch(setUser(data.user));
-   return response;
+export const thunkRestoreUser = () => async (dispatch) => {
+   const res = await csrfFetch("/api/session");
+   const data = await res.json();
+   dispatch(actionSetUser(data.user));
+   return res;
+};
+
+export const thunkSignUp = (user) => async (dispatch) => {
+   const { username, firstName, lastName, email, password } = user;
+   const res = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+         username,
+         firstName,
+         lastName,
+         email,
+         password,
+      }),
+   });
+   const data = await res.json();
+   dispatch(actionSetUser(data.user));
+   return res;
 };
 
 const initialState = { user: null };
