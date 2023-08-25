8257c9cd498985b1e6311fc8a1954e82c36e7bce
Ryan Fournier <ryanfour1637@gmail.com>
2023-08-25 06:45:56 -0700

    added signup form modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
+import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -9,19 +8,13 @@ function App() {
    const dispatch = useDispatch();
    const [isLoaded, setIsLoaded] = useState(false);
    useEffect(() => {
-      dispatch(sessionActions.thunkRestoreUser()).then(() => setIsLoaded(true));
+      dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
    }, [dispatch]);
 
    return (
       <>
          <Navigation isLoaded={isLoaded} />
-         {isLoaded && (
-            <Switch>
-               <Route path="/signup">
-                  <SignupFormPage />
-               </Route>
-            </Switch>
-         )}
+         {isLoaded && <Switch></Switch>}
       </>
    );
 }
