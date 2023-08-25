5fc8f2f0984b9f0797385ee002553bc66a9f6826
Ryan Fournier <ryanfour1637@gmail.com>
2023-08-25 14:08:13 -0700

    started spots reducer

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,10 +1,45 @@
 import { csrfFetch } from "./csrf";
 
-const SET_SPOTS = "spots/setSpots";
+const READ_ALL_SPOTS = "spots/actionReadSpots";
+const READ_ONE_SPOT = "spots/actionReadOneSpot";
 
-const actionSetSpots = (spots) => {
+const actionReadSpots = (spots) => {
    return {
-      type: SET_SPOTS,
+      type: READ_ALL_SPOTS,
       payload: spots,
    };
 };
+
+const actionReadOneSpot = (spot) => {
+   return {
+      type: READ_ONE_SPOT,
+      payload: spot,
+   };
+};
+
+export const thunkReadSpots = () => async (dispatch) => {
+   const res = await csrfFetch("/api/spots");
+   const data = await res.json();
+   dispatch(actionReadSpots(data.Spots));
+};
+
+export const thunkReadOneSpot = () => async (dispatch) => {
+   const res = await c;
+};
+
+const initialState = { Spots: {} };
+
+const spotsReducer = (state = initialState, action) => {
+   let newState;
+   switch (action.type) {
+      case READ_SPOTS:
+         // make it into an object and normalize
+         newState = Object.assign({}, state);
+         action.payload.forEach((spot) => (newState[spot.id] = spot));
+         return newState;
+      default:
+         return state;
+   }
+};
+
+export default spotsReducer;
